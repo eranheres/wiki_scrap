da70abe926855d246f3680804dd47974328f2fef
eran.heres@gmail.com <eran.heres@gmail.com>
2020-06-10 16:10:37 +0300

    support heb

--- scrap_wiki_list.py
+++ scrap_wiki_list.py
@@ -3,17 +3,21 @@ import wptools
 import requests
 import pathlib
 
+lang = 'he'
+
 def get_category_pages(category):
-    wiki_cat = wptools.category(category)
+    wiki_cat = wptools.category(category, lang=lang)
     wiki_cat.get_members()
     results = []
-    for topic in wiki_cat.data['members'][:5]:
+    for topic in wiki_cat.data['members']:
         title = topic['title']
         try:
-            page = wptools.page(title).get_query()
+            page = wptools.page(title, lang=lang).get_query()
         except ValueError:
             continue
         thumbimg = page.images(fields='url', token='thumb')
+        if thumbimg is None or len(thumbimg) == 0 or thumbimg[0]['url'] is None:
+            continue
         url = thumbimg[0]['url']
         results.append({"title": title, "url":url})
     return results
@@ -27,7 +31,10 @@ def download_image(image_name, url):
 
 
 category = 'Category:Best_Actor_Academy_Award_winners'
+category = 'קטגוריה:כתבים_מדיניים_ישראלים'
 results = get_category_pages(category)
-print(results)
 for result in results:
+    print(result)
     download_image(result["title"], result["url"])
+u = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Benny_morris.jpg/214px-Benny_morris.jpg'
+download_image("t", u)
